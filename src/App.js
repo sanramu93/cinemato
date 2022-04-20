96b4cc4840245391af2d41555b07b46347f3be64
Santiago <sanramu93@gmail.com>
2022-04-20 12:03:17 -0500

    Change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import { getMovies, getMovieGenres, getImage } from "./apis/tmdbAPI";
 
