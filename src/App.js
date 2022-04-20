3abe54e0a70ef712cfdc0136f22c6f14f2c72909
Santiago <sanramu93@gmail.com>
2022-04-20 15:11:37 -0500

    Add BrowserRouter again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { getMovies, getMovieGenres, getImage } from "./apis/tmdbAPI";
 
